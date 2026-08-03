6964e416343865b3048e508e12a59e79f24c5650
Ivy Reese <ivyreese@fastmail.com>
2026-08-03 16:39:51 -0600

    Fix deadname

--- system/server.ts
+++ system/server.ts
@@ -1,7 +1,7 @@
 // Server
 // This is a super-simple live-reloading web server for local development,
 // using websockets & an injected <script> tag to trigger browser refresh.
-// For more, see the original: https://github.com/ivanreese/please-reload.
+// For more, see the original: https://github.com/ivyreese/please-reload.
 // If our needs change, it'll be easy to replace this with something else.
 
 import { Buffer } from "node:buffer"
